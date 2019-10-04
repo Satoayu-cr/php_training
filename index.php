2014822a982a0f9a3cfa8212c27cd450031bc1fb
ayu-sato <a-sato@cries.co.jp>
2019-10-04 14:45:33 +0900

    q_1_20191004

--- index.php
+++ index.php
@@ -11,6 +11,13 @@ header('Content-Type: text/html; charset=UTF-8');
     <head>
         <title>会員限定ページ</title>
     </head>
+
+    <style type="text/css">
+        th{
+            background-color:#FFFFCC;
+        }
+    </style>
+
     <body>
         <h1>
             ようこそ,<?=h($_SESSION['username'])?>さん
@@ -25,20 +32,24 @@ $dsn = 'pgsql:dbname=php_training_database host=172.20.0.35 port=5432';
 $user = 'php_training_user';
 $password = '7890';
 
+print('<table border = "1" style="border-collapse: collapse"><tr>');
+print('<th>図鑑番号</th><th>名前</th><th>タイプ１</th><th>タイプ2</th></tr>');
+
 try {
     $dbh = new PDO($dsn, $user, $password);
     $sql = 'select * from pokemon_status';
+
     foreach ($dbh->query($sql) as $row) {
-        print($row['dict_num']);
-        print($row['name']);
-        print($row['type1']);
-        print($row['type2'].'<br>');
+        print('<td>'.$row['dict_num'].'</td>');
+        print('<td>'.$row['name'].'</td>');
+        print('<td>'.$row['type1'].'</td>');
+        print('<td>'.$row['type2'].'</td></tr>');
     }
 } catch (PDOException $e) {
     print('Error:'.$e->getMessage());
     die();
 }
-
+print('</table>');
 
 $dbh = null;
 
